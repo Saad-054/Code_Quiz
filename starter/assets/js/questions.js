1f998dc3dec5c75272a9071c32dcd0474c284839
unknown <saad.siddiqui@msn.com>
2023-07-28 18:52:09 +0100

    added questions to questions.js

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        question: "Question 1",
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: "Option 1"
+    },
+    {
+        question: "Question 1",
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: "Option 1"
+    },
+    {
+        question: "Question 1",
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: "Option 1"
+    },
+    {
+        question: "Question 1",
+        choices: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: "Option 1"
+    },
+]
