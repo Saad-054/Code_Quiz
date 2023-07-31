35b97def9207b86897544b7dfba6665a981a1bd3
unknown <saad.siddiqui@msn.com>
2023-07-31 15:23:28 +0100

    code for scores.js file

---
+++ starter/assets/js/scores.js
@@ -0,0 +1,18 @@
+var highscoresE1 = document.getElementById("highscores");
+var clearBtnE1 - document.getElementById("clear");
+
+function displayHighScores() {
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    highScores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    for (var i = 0; i < highScores.length; i++) {
+        var scoreData = highScores[i];
+        var liE1 = document.createElement("li");
+        liE1.textContent = scoreData.iniitals + " - " + scoreData.score;
+        highscoresE1.appendChild(liE1);
+    }
+}
+
