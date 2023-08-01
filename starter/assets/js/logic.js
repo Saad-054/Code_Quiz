53406262d8d8c39d6362168475f5192eff4b84b9
unknown <saad.siddiqui@msn.com>
2023-08-01 23:37:16 +0100

    added final comments to logic.js file

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,22 +1,38 @@
+// adds event listenertot he document object. When DOMContentLoaded eveent occurs, event listener will be executed
 document.addEventListener("DOMContentLoaded", function() {
+
+    // variables related to timer, seconds, questionindex and score
     var timer;
     var timeLeft = 60;
     var currentQuestionIndex = 0;
     var score = 0;
 
+    // used for conatainer holding quiz content
     var containerEl = document.querySelector(".container");
+    // used for timer displayed on page
     var timerEl = document.querySelector("#time");
+    // used for start screen of quiz
     var startScreenEl = document.getElementById("start-screen");
+    // used for question eleemnt content
     var questionsEl = document.getElementById("questions");
+    // used for displaying question
     var questionTitleEl = document.getElementById("question-title");
+    // used for holding answer choices
     var choicesEl = document.getElementById("choices");
+    // used to provide feedback once answer seleected
     var feedbackEl = document.getElementById("feedback");
+    // used for end screen when quiz completed
     var endScreenEl = document.getElementById("end-screen");
+    // used for final score being displayed
     var finalScoreEl = document.getElementById("final-score");
+    // used for button for putting initials for scores
     var initialsInputEl = document.getElementById("initials");
+    // used for button for submitting initial for score
     var submitBtnEl = document.getElementById("submit");
+    // used for link to select highscores
     var highscoresLinkEl = document.querySelector(".scores a");
 
+    // function for starting quiz and timer countdown
     function startQuiz() {
         if (containerEl) {
             containerEl.style.display = "block";
@@ -36,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
         displayQuestion();
     }
 
+    // fucntion for displaying question
     function displayQuestion() {
         questionsEl.classList.remove ("hide")
         var currentQuestion = questions[currentQuestionIndex];
@@ -52,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    // function for user's answer selection. If incorrect, timer goes down by 10
     function checkAnswer(event) {
         var selectedChoice = event.target.textContent
         var currentQuestion = questions[currentQuestionIndex];
@@ -68,6 +86,7 @@ document.addEventListener("DOMContentLoaded", function() {
         displayQuestion();
     }
 
+    // function for end of quiz and displayign final score
     function endQuiz() {
         clearInterval(timer);
         if (questionsEl) {
@@ -79,6 +98,7 @@ document.addEventListener("DOMContentLoaded", function() {
         finalScoreEl.textContent = score;
     }
 
+    // function for saving the high score with uder's initials
     function saveHighScore() {
         var initials = initialsInputEl.value.trim();
 
@@ -96,15 +116,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    // start button add events listner callign startquiz function when clicked
     var startBtn = document.getElementById("start");
     if (startBtn) {
         startBtn.addEventListener("click", startQuiz);
     }
 
+    // adds event listener calling the saveHighScore when clicked
     if (submitBtnEl) {
         submitBtnEl.addEventListener("click", saveHighScore);
     }
 
+    // redirects to highscores
     if (highscoresLinkEl) {
         highscoresLinkEl.addEventListener("click", function(event) {
             event.preventDefault();
